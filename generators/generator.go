d73e932e2fdb11f171d442063a5d0dbf1dee59c4
Sunchiii <rachanlamoolphal@gmail.com>
2024-06-03 14:20:12 +0700

    `chore: Update file paths in generator functions`

--- generators/generator.go
+++ generators/generator.go
@@ -22,7 +22,11 @@ func GenerateInitialStructure() {
 		os.Exit(1)
 	}
 	CreateConfigEnv(projectName)
+	CreateLoggers(projectName)
+	CreatePagination(projectName)
 	CreateAppErrs()
+	CreateRoutes()
+	CreateFiberRoutes(projectName)
 }
 
 func CreateConfigEnv(projectName string) {
@@ -166,6 +170,216 @@ func CreateAppErrs() {
 		fmt.Fprintf(destination, "}\n")
 
 		fmt.Println("Created AppErrs successfully", file)
+	} else {
+		fmt.Println("File already exists!", file)
+	}
+}
+
+func CreateLoggers(projectName string) {
+	pathFolder := "logs"
+	if _, err := os.Stat(pathFolder); errors.Is(err, os.ErrNotExist) {
+		err := os.Mkdir(pathFolder, os.ModePerm)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	path := "logs/"
+	file := path + "loggers.go"
+	var _, err = os.Stat(file)
+
+	if os.IsNotExist(err) {
+		destination, err := os.Create(file)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer destination.Close()
+
+		// Write the logging package code to the file
+		fmt.Fprintf(destination, "package logs\n\n")
+		fmt.Fprintf(destination, "import (\n")
+		fmt.Fprintf(destination, "    \"fmt\"\n")
+		fmt.Fprintf(destination, "    \"go.uber.org/zap\"\n")
+		fmt.Fprintf(destination, "    \"go.uber.org/zap/zapcore\"\n")
+		fmt.Fprintf(destination, ")\n\n")
+		fmt.Fprintf(destination, "var log *zap.Logger\n")
+		fmt.Fprintf(destination, "var err error\n\n")
+		fmt.Fprintf(destination, "func init() {\n")
+		fmt.Fprintf(destination, "    config := zap.NewProductionConfig()\n")
+		fmt.Fprintf(destination, "    config.EncoderConfig.TimeKey = \"timestamp\"\n")
+		fmt.Fprintf(destination, "    config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder\n")
+		fmt.Fprintf(destination, "    config.EncoderConfig.StacktraceKey = \"\"\n")
+		fmt.Fprintf(destination, "    log, err = config.Build(zap.AddCallerSkip(1))\n")
+		fmt.Fprintf(destination, "    if err != nil {\n")
+		fmt.Fprintf(destination, "        fmt.Println(err)\n")
+		fmt.Fprintf(destination, "        return\n")
+		fmt.Fprintf(destination, "    }\n")
+		fmt.Fprintf(destination, "}\n\n")
+		fmt.Fprintf(destination, "func Info(message string, fields ...zap.Field) {\n")
+		fmt.Fprintf(destination, "    log.Info(message, fields...)\n")
+		fmt.Fprintf(destination, "}\n\n")
+		fmt.Fprintf(destination, "func Error(message interface{}, fields ...zap.Field) {\n")
+		fmt.Fprintf(destination, "    switch v := message.(type) {\n")
+		fmt.Fprintf(destination, "    case error:\n")
+		fmt.Fprintf(destination, "        log.Error(v.Error(), fields...)\n")
+		fmt.Fprintf(destination, "    case string:\n")
+		fmt.Fprintf(destination, "        log.Error(v, fields...)\n")
+		fmt.Fprintf(destination, "    }\n")
+		fmt.Fprintf(destination, "}\n\n")
+		fmt.Fprintf(destination, "func Debug(message string, fields ...zap.Field) {\n")
+		fmt.Fprintf(destination, "    log.Debug(message, fields...)\n")
+		fmt.Fprintf(destination, "}\n")
+
+		fmt.Println("Created Loggers successfully", file)
+	} else {
+		fmt.Println("File already exists!", file)
+		return
+	}
+}
+
+func CreatePagination(projectName string) {
+	pathFolder := "paginates"
+	if _, err := os.Stat(pathFolder); os.IsNotExist(err) {
+		err := os.Mkdir(pathFolder, os.ModePerm)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	path := pathFolder + "/"
+	file := path + "pagination.go"
+	var _, err = os.Stat(file)
+
+	if os.IsNotExist(err) {
+		destination, err := os.Create(file)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer destination.Close()
+
+		fmt.Fprintf(destination, "package paginates\n\n")
+		fmt.Fprintf(destination, "import (\n")
+		fmt.Fprintf(destination, "	\"gorm.io/gorm\"\n")
+		fmt.Fprintf(destination, "	\"gorm.io/gorm/clause\"\n")
+		fmt.Fprintf(destination, ")\n\n")
+		fmt.Fprintf(destination, "type PaginateRequest struct {\n")
+		fmt.Fprintf(destination, "	Item int `json:\"item\" validate:\"required\"`\n")
+		fmt.Fprintf(destination, "	Page int `json:\"page\" validate:\"required\"`\n")
+		fmt.Fprintf(destination, "}\n\n")
+		fmt.Fprintf(destination, "type PaginatedResponse struct {\n")
+		fmt.Fprintf(destination, "	Data        interface{} `json:\"data\"`\n")
+		fmt.Fprintf(destination, "	Total       int         `json:\"total\"`\n")
+		fmt.Fprintf(destination, "	PerPage     int         `json:\"per_page\"`\n")
+		fmt.Fprintf(destination, "	CurrentPage int         `json:\"current_page\"`\n")
+		fmt.Fprintf(destination, "	LastPage    int         `json:\"last_page\"`\n")
+		fmt.Fprintf(destination, "}\n\n")
+		fmt.Fprintf(destination, "func Paginate(db *gorm.DB, model interface{}, paginate PaginateRequest) (*PaginatedResponse, error) {\n")
+		fmt.Fprintf(destination, "	var total int64\n")
+		fmt.Fprintf(destination, "	db.Model(model).Count(&total)\n")
+		fmt.Fprintf(destination, "	lastPage := (int(total) + paginate.Item - 1) / paginate.Item\n")
+		fmt.Fprintf(destination, "	offset := (paginate.Page - 1) * paginate.Item\n")
+		fmt.Fprintf(destination, "	result := db.Preload(clause.Associations).Limit(paginate.Item).Offset(offset).Find(model)\n")
+		fmt.Fprintf(destination, "	if result.Error != nil {\n")
+		fmt.Fprintf(destination, "		return nil, result.Error\n")
+		fmt.Fprintf(destination, "	}\n")
+		fmt.Fprintf(destination, "	pagination := &PaginatedResponse{\n")
+		fmt.Fprintf(destination, "		Total:       int(total),\n")
+		fmt.Fprintf(destination, "		PerPage:     paginate.Item,\n")
+		fmt.Fprintf(destination, "		CurrentPage: paginate.Page,\n")
+		fmt.Fprintf(destination, "		LastPage:    lastPage,\n")
+		fmt.Fprintf(destination, "		Data:        model,\n")
+		fmt.Fprintf(destination, "	}\n")
+		fmt.Fprintf(destination, "	return pagination, nil\n")
+		fmt.Fprintf(destination, "}\n")
+
+		fmt.Println("Created Pagination successfully:", file)
+	} else {
+		fmt.Println("File already exists!", file)
+	}
+}
+
+func CreateRoutes() {
+	pathFolder := "routes"
+	if _, err := os.Stat(pathFolder); os.IsNotExist(err) {
+		err := os.Mkdir(pathFolder, os.ModePerm)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	path := pathFolder + "/"
+	file := path + "routes.go"
+	var _, err = os.Stat(file)
+
+	if os.IsNotExist(err) {
+		destination, err := os.Create(file)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer destination.Close()
+
+		fmt.Fprintf(destination, "package routes\n\n")
+		fmt.Fprintf(destination, "import \"github.com/gofiber/fiber/v2\"\n\n")
+		fmt.Fprintf(destination, "type Routes interface {\n")
+		fmt.Fprintf(destination, "	Install(app *fiber.App)\n")
+		fmt.Fprintf(destination, "}\n")
+
+		fmt.Println("Created Routes successfully:", file)
+	} else {
+		fmt.Println("File already exists!", file)
+	}
+}
+
+func CreateFiberRoutes(projectName string) {
+	pathFolder := "routes"
+	if _, err := os.Stat(pathFolder); os.IsNotExist(err) {
+		err := os.Mkdir(pathFolder, os.ModePerm)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	path := pathFolder + "/"
+	file := path + "fiber_routes.go"
+	var _, err = os.Stat(file)
+
+	if os.IsNotExist(err) {
+		destination, err := os.Create(file)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer destination.Close()
+
+		fmt.Fprintf(destination, "package routes\n\n")
+		fmt.Fprintf(destination, "import (\n")
+		fmt.Fprintf(destination, "	\"%s/controllers\"\n", projectName)
+		fmt.Fprintf(destination, "	\"github.com/gofiber/fiber/v2\"\n")
+		fmt.Fprintf(destination, ")\n\n")
+		fmt.Fprintf(destination, "type fiberRoutes struct {\n")
+		fmt.Fprintf(destination, "	controller controllers.Controller\n")
+		fmt.Fprintf(destination, "}\n\n")
+		fmt.Fprintf(destination, "func (r fiberRoutes) Install(app *fiber.App) {\n")
+		fmt.Fprintf(destination, "	route := app.Group(\"web/\", func(ctx *fiber.Ctx) error {\n")
+		fmt.Fprintf(destination, "		return ctx.Next()\n")
+		fmt.Fprintf(destination, "	})\n")
+		fmt.Fprintf(destination, "	route.Post(\"hello\", r.controller.StartController)\n")
+		fmt.Fprintf(destination, "}\n\n")
+		fmt.Fprintf(destination, "func NewWebRoutes(\n")
+		fmt.Fprintf(destination, "	controller controllers.Controller,\n")
+		fmt.Fprintf(destination, ") Routes {\n")
+		fmt.Fprintf(destination, "	return &fiberRoutes{\n")
+		fmt.Fprintf(destination, "		controller: controller,\n")
+		fmt.Fprintf(destination, "	}\n")
+		fmt.Fprintf(destination, "}\n")
+
+		fmt.Println("Created fiber_routes.go successfully:", file)
 	} else {
 		fmt.Println("File already exists!", file)
 	}
