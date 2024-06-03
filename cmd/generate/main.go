96b4fc2b82b0bb3336ebae6212876d964d6473e1
Sunchiii <rachanlamoolphal@gmail.com>
2024-06-03 15:40:19 +0700

    `chore: Update file paths in generator functions`

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/Sunchiii/go-module-generator/generators"
 )
@@ -16,8 +19,26 @@ func main() {
 	}
 
 	moduleName := flag.Arg(0)
+	var projectName string
 
 	if moduleName == "init" {
+		fmt.Print("Enter Project Name: ")
+		fmt.Scan(&projectName)
+
+		// validate project name must not empty and must not contain space
+		if projectName == "" {
+			log.Fatal("Project name must not empty")
+		}
+
+		if le := strings.Split(projectName, " "); len(le) > 1 {
+			log.Fatal("Project name must not contain space")
+		}
+
+		cmd := exec.Command(fmt.Sprintf("go mod init %s", projectName))
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}
+
 		generators.GenerateInitialStructure()
 	} else {
 		generators.GenerateModules(moduleName)
